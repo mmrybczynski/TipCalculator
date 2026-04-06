232d74562e612caa4bab137e8c5387c60ec7f07c
Mateusz <mateusz.rybczynski@icloud.com>
2026-04-06 20:35:06 +0200

    Created background view

---
+++ TipCalculator/Components/BackgroundView.swift
@@ -0,0 +1,39 @@
+//
+//  BackgroundView.swift
+//  TipCalculator
+//
+//  Created by Mateusz Rybczyński on 4/6/2026.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    @State var selectedBackground: Int
+    var body: some View {
+        backgroundForSelection(selectedBackground)
+            .ignoresSafeArea()
+    }
+    @ViewBuilder
+    private func backgroundForSelection(_ value: Int) -> some View {
+        switch value {
+        case 0:
+            Color.blue
+        case 1:
+            LinearGradient(colors: [.purple, .pink], startPoint: .topLeading, endPoint: .bottomTrailing)
+        case 2:
+            RadialGradient(gradient: Gradient(colors: [.orange, .red]),
+                           center: .center,
+                           startRadius: 20,
+                           endRadius: 300)
+        case 3:
+            AngularGradient(gradient: Gradient(colors: [.mint, .teal, .indigo, .mint]),
+                            center: .center)
+        default:
+            Color.gray.opacity(0.2)
+        }
+    }
+}
+
+#Preview {
+    BackgroundView(selectedBackground: 0)
+}
