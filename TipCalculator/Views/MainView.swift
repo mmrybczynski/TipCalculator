708567ae0c34b4933c17397778fd7127f6bfc705
Mateusz <mateusz.rybczynski@icloud.com>
2026-04-06 20:54:08 +0200

    Created main view with background

--- TipCalculator/Views/MainView.swift
+++ TipCalculator/Views/MainView.swift
@@ -8,8 +8,17 @@
 import SwiftUI
 
 struct MainView: View {
+    @AppStorage("selectedOption") private var selectedOption:Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            BackgroundView(selectedBackground: selectedOption)
+            VStack {
+                Text("\(selectedOption)")
+                
+            }
+        }
+        
     }
 }
 
