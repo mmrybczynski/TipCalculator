ec061d92acdbfa53adb9da2a5adb1ce9425183a8
Mateusz <mateusz.rybczynski@icloud.com>
2026-04-13 21:33:22 +0200

    Created main view with fields of amount, pople and tip. Additionally two previews with acces granted and denied to contacts

--- TipCalculator/Views/MainView.swift
+++ TipCalculator/Views/MainView.swift
@@ -9,19 +9,66 @@ import SwiftUI
 
 struct MainView: View {
     @StateObject private var settings = AppSettings.shared
+    @State private var selectedTip:Int = 5
+    
+    private let tips:[Int] = [0,5,10, 15, 20, 25, 30]
     
     var body: some View {
         ZStack {
             BackgroundView(selectedBackground: settings.selectedOption)
-            VStack {
-                Text("\(settings.selectedOption)")
+            VStack(alignment: .leading) {
+                if settings.contactAccess {
+                    Text("access")
+                }
+                HStack {
+                    Text(settings.selectedCurrency)
+                        .padding(.trailing, 5)
+                    TextField("Podaj kwotę", text: .constant(""))
+                        .keyboardType(.decimalPad)
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 18, style: .continuous))
+                .padding(.bottom,10)
+                
+                if settings.contactAccess {
+                    Text("access")
+                } else {
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .padding(.trailing, 5)
+                        TextField("Ile osób", text: .constant(""))
+                            .keyboardType(.numberPad)
+                    }
+                    .padding()
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 18, style: .continuous))
+                    .padding(.bottom,10)
+                }
+                
+                Text("Jaki tip?")
+                Picker("Tip", selection: $selectedTip) {
+                    ForEach(tips, id: \.self) { tip in
+                        Text("\(tip)%").tag(tip)
+                    }
+                }
+                .pickerStyle(.segmented)
                 
             }
+            .padding()
         }
         
     }
 }
 
-#Preview {
-    MainView()
+#Preview("Access granted") {
+    let s = AppSettings.shared
+    s.contactAccess = true
+    return MainView()
+}
+
+#Preview("Access denied") {
+    let s = AppSettings.shared
+    s.contactAccess = false
+    return MainView()
 }
